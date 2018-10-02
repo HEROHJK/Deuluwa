c9551f5abc67bc9e5c941a5cbe637731fb7f13e4
herohjk <herohjk@herohjk.com>
2018-10-02 14:41:27 +0900

    수업정보 껍데기 추가 웹서버에서 기존 레스트API삭제 사용자정보 없을때 에러페이지 나던 오류 수정

--- WebServer/deuluwa/views.py
+++ WebServer/deuluwa/views.py
@@ -19,6 +19,8 @@ def getUserInfo(request):
 
         if len(list(User.objects.raw(command))) > 0:
             message = 'success'
+        else:
+            message = 'failed'
 
     except Exception as e:
         print("실패 원인 : " + str(e))
